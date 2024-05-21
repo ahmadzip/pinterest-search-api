940ad8a79bc145dfcc128f12872e066e85ec46ba
ahmadzip <ahmad.5220411084@student.uty.ac.id>
2024-05-21 17:51:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+const { search } = require("pinterest-dl");
+
+app.get("/", (req, res) => {
+  const params = req.query.q;
+  search(params)
+    .then((data) => {
+      res.send(data[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ error: "An error occurred while fetching data" });
+    });
+});
+
+app.listen(PORT, () => {
+  console.log("http://localhost:3000/?q=cat");
+  console.log("Server Listening on PORT:", PORT);
+});
